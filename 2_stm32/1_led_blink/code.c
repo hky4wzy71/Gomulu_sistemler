56dd3d627c4e596f1baf37a8fc3474a9afbca551
hky4wzy71 <74815003+hky4wzy71@users.noreply.github.com>
2020-12-13 21:35:10 +0200

    Rename code.txt to code.c

--- 2_stm32/1_led_blink/code.c
+++ 2_stm32/1_led_blink/code.c
@@ -5,23 +5,23 @@ int main(void){
 	// reset clock control(RCC) ile clock veya resetini kontrol ediyoruz.
 	// RCC_APB2ENR registeri ile de portlarin clocklarini ayarlicaz.
   // APB2ENR, 00000000000000000000000000000000	 32 bit  bu regin 4. bitini 1 yaparsak c portunun clocku aktif hale gelcek.
-	// (1<<4),  00000000000000000000000000001000   bu maske ile 4. bite yazdirmaya çalisiyoruz (stmManuel 133sayfa) 
+	// (1<<4),  00000000000000000000000000001000   bu maske ile 4. bite yazdirmaya Ã§alisiyoruz (stmManuel 133sayfa) 
 	//          00000000000000000000000000001000 or ile 4. bite 1 setlemis olduk artik c portunun clksi aktif
 	
 	
 	GPIOC -> GPIOC -> CRH &= ~((1<<23)|(1<<22)); 
 	//port c aktif hale geldi artik 13.pini aktif yapalim.GPIOC ile genel i/o controlunu yapiyouz.
-  // CRH ise 8-15 arasi  bitleri kontrol ediyo 13.pin için crh kullancaz.
-	//CRHta mode var bide cnf(control fonksiyon) var mode 23 22 ile cnf ayarliyoruz. mode output için 00 push-pull output yapcaz.
+  // CRH ise 8-15 arasi  bitleri kontrol ediyo 13.pin iÃ§in crh kullancaz.
+	//CRHta mode var bide cnf(control fonksiyon) var mode 23 22 ile cnf ayarliyoruz. mode output iÃ§in 00 push-pull output yapcaz.
 	// 13.pinin cnfsini ayarliyalim.
 	//crh,      00000000000000000000000000000000
-	//(1<<23),  00000000100000000000000000000000 23. bitine 1 set etmek için maske
+	//(1<<23),  00000000100000000000000000000000 23. bitine 1 set etmek iÃ§in maske
 	//~((1<<23),11111111011111111111111111111111
-	//CRH &= ~((1<<23), böylece 23.pini 0 lamis olduk
+	//CRH &= ~((1<<23), bÃ¶ylece 23.pini 0 lamis olduk
 	
 	
 	GPIOC -> CRH |= ((1<<21)|(1<<20));
-	// 13. pinin modunu ayarlayalim out put için 
+	// 13. pinin modunu ayarlayalim out put iÃ§in 
 	//21 ve 20yi 1 yapcaz (stmManuel'den yine bakilirsa)
 	//CRH, 			00000000000000000000000000000000
 	//(1<<21),	00000000010000000000000000000000
@@ -29,7 +29,7 @@ int main(void){
 while(1){
 	
 	GPIOC ->ODR |= (1<<13);
-	//Output Data regin(ODR) ile çikisi kontrol ediyoruz bizim led 13. pinde
+	//Output Data regin(ODR) ile Ã§ikisi kontrol ediyoruz bizim led 13. pinde
 	// (1<<13) maskesi ile 00000000000000000010000000000000 
 	// ODR,								 00000000000000000000000000000000
 	//ODR |= (1<<13) 			 00000000000000000010000000000000 set etmis olduk 13. pine 3.3 volt vercek artik.
