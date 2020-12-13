b65d2d95410f6a991fa3525dc00c761918331f9a
hky4wzy71 <74815003+hky4wzy71@users.noreply.github.com>
2020-12-13 21:36:20 +0200

    Rename New Text Document.txt to code.c

--- 2_stm32/2_System_click/code.c
+++ 2_stm32/2_System_click/code.c
@@ -15,11 +15,11 @@ int main(){
 	RCC -> APB2ENR |= (1<<4);
 	GPIOC -> CRH |=  (1<<20)|(1<<21);
 	GPIOC -> CRH &=~ (1<<22)|(1<<23);
-	SysTick -> LOAD =RCC_CFGR_MCO_SYSCLK/8; 													//72Mhz/8 = 9.000.000 == 100010010101010001000000 bu degeri load registerina yüklüyoruz asagi saymaya baslicak 0 oldugunda 1sn geçmis olcak
+	SysTick -> LOAD =RCC_CFGR_MCO_SYSCLK/8; 													//72Mhz/8 = 9.000.000 == 100010010101010001000000 bu degeri load registerina yÃ¼klÃ¼yoruz asagi saymaya baslicak 0 oldugunda 1sn geÃ§mis olcak
 	SysTick->CTRL=SysTick_CTRL_ENABLE_Msk|SysTick_CTRL_TICKINT_Msk;  //SysTick_CTRL_ENABLE_Msk = 1UL(0..001) | SysTick_CTRL_TICKINT_Msk = 1UL(0.001)  unsigned long
 	while(1){
-	/* program sonsuz döngüye girrmis oldu.
-		ama sayaç 0 oldgunda Systick interupt olusturcak ve SysTick_Handler fonksiyonunu çalistircak.
+	/* program sonsuz dÃ¶ngÃ¼ye girrmis oldu.
+		ama sayaÃ§ 0 oldgunda Systick interupt olusturcak ve SysTick_Handler fonksiyonunu Ã§alistircak.
 	*/
 	}
 }
